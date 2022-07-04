c626666dbe81bddfdf4e992180d549d143a52986
moraamoseti2 <mosetimoraamartex@gmail.com>
2022-07-04 12:48:17 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
  /**
-         * this is acomment
-         * **/
+  * This displays the line below
+  * **/
 int main(void)
 {
-	
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
