ef38dcc3e1d531928dd74b1b58cc33017420abd1
moraamoseti2 <mosetimoraamartex@gmail.com>
2022-07-10 09:32:23 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+ * main function to print letters except e and q
+ * return: 0 success
+*/
+int main(void)
+{
+	char ch;
+	for (ch='a'; ch<='z' ;ch++)
+	{
+		if(ch!='e' && ch!='q')
+			putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
