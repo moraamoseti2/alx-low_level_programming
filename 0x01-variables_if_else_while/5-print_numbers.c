a0e8b40ffde1472186c3867a736962a567cf62c5
moraamoseti2 <mosetimoraamartex@gmail.com>
2022-07-10 09:47:25 -0700

    Task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main function to print base numbers base 10 between 0 and 10
+ * return :0 success
+*/
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10 ; a++)
+		printf("%d", a);
+	printf("\n");
+	return (0);
+}
