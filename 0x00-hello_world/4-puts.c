ab914245b6bf86d754bfea5f2af47d15fadace2f
moraamoseti2 <mosetimoraamartex@gmail.com>
2022-07-10 03:13:27 -0700

    Task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /** 
- * main- function that directs puts command
- * */
+ * main-prints the size of various types
+ * Return:0 if exited properly, non-zerootherwise
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
