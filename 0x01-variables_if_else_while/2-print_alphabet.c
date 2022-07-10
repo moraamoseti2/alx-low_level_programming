bbfaa1750d80b17682334db0c69d7e9175923af5
moraamoseti2 <mosetimoraamartex@gmail.com>
2022-07-10 06:30:40 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/** 
+ * main function that prints lower case alphabet letters
+ * return: 0 success
+*/
+int main(void)
+{
+	int ch;
+	 for (ch='a'; ch<='z'; ch++)
+	 {
+		 putchar(ch);
+	         putchar ("\n");
+	 }
+	 return (0);
+}
