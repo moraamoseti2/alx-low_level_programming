da31e72bcc7db8bd1c514ed02b7097192b207d2c
moraamoseti2 <mosetimoraamartex@gmail.com>
2022-07-10 10:00:00 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main function to print single numbers
+ * return 0 :success
+*/
+int main(void)
+{
+	int a=48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
