0046473d509d754298e0eb9aa43b3dd2ee86a191
moraamoseti2 <mosetimoraamartex@gmail.com>
2022-07-10 10:46:13 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main function to print all possible combinations of single-digit numbers
+ * return 0 success
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 48 ; i < 58 ; i++)
+		putchar(i);
+	if(i != 57)
+	{
+		putchar(',');
+	        putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
+
+
