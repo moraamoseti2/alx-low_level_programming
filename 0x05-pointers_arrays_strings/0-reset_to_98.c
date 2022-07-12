cc53c1fd1be3ba10b86117d6e484991aae0fa57e
moraamoseti2 <mosetimoraamartex@gmail.com>
2022-07-12 04:37:06 -0700

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * function pointer to an int as parameter and updates the value it points to to 98
+ * function parameter and updates the value it points to to 98
  * Return: 0 success
 */
 void reset_to_98(int *n)
