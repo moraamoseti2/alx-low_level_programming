da8fdcc0071243d93c1513913f8439a328c2e529
moraamoseti2 <mosetimoraamartex@gmail.com>
2022-07-13 03:00:37 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- * main: check the code
+ * print_alphabet: check the code
  * description: print alphabet 10times
  * return : always 0
 */
 void print_alphabet_x10(void)
 {
 	int n, c;
+
 	c = 0;
 
 	while (c < 10)
