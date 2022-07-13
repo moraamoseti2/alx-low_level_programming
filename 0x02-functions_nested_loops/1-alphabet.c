fe899ce03d782f6061e9432330066435cce34bf8
moraamoseti2 <mosetimoraamartex@gmail.com>
2022-07-13 00:06:09 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * print_alphabet main: entry point
- * Description-alphabet in lowercase 
+ * main: entry point
+ * Description: alphabet in lowercase
  * return : always 0 success
 */
 void print_alphabet(void) /* print_alphabet function */
