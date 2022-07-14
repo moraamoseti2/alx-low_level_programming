e5fd06a6d7879f8c3dbc2847ffcb8379a547c3e5
moraamoseti2 <mosetimoraamartex@gmail.com>
2022-07-13 23:50:46 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,13 +10,14 @@ char *_strcat(char *dest, char *src)
 	int length, j;
 	
 	length = 0;
-	while (dest[length] != '\0')
-	{
+	while (dest[length)
+	
 		length++;
-	}
+	
 	for (j = 0; src[j] != '\0'; j++, length++)
 	{
 		dest[length] = src[j];
+		len += 1;
 	}
 	dest[length] = '\0';
 	return (dest);
