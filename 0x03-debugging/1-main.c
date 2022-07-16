b538b4e7a33fd318219d2993726d059eff96a1c6
moraamoseti2 <mosetimoraamartex@gmail.com>
2022-07-16 05:51:07 -0700

    Task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,6 +10,7 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
+
 	/**
 	 * while (i < 10)
 	 * {
@@ -17,5 +18,6 @@ int main(void)
 	 * }
 	*/
 	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
