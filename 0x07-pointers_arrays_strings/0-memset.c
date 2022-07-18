ed738a2e563905b9545aba22b8df806c1a4cd02a
moraamoseti2 <mosetimoraamartex@gmail.com>
2022-07-18 07:21:21 -0700

    Commit task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,11 +1,12 @@
 #include "main.h"
+#include <string.h>
 /**
  * char *_memset function
  *return pointer
 */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int q;
+	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
