67efff994507ba33af109044315bf1c16de3afc9
moraamoseti2 <mosetimoraamartex@gmail.com>
2022-07-18 09:30:29 -0700

    Task 6 commit

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -14,14 +14,8 @@ void print_chessboard(char (*a)[8])
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
-		{
-			if (j == 7)
-			{
-				_putchar(a[i][j]);
-				_putchar('\n');
-			}
-			else
-				_putchar(a[i][j]);
-		}
+		_putchar(a[i][j]);
+		_putchar('\n');
+
 	}
 }
