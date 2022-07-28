4d8254c21037530a2ac01fa3c4aea9d18abfeaf3
moraamoseti2 <mosetimoraamartex@gmail.com>
2022-07-28 04:16:55 -0700

    Task 0 commit

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,12 +2,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * malloc_checked -> checking malloc
- * @b : param b
+ * malloc_checked - function that allocates memory using malloc.
+ *
+ * @b: This is the length to allocate in the memory
+ *
+ * Return: Returns a pointer to the allocated memory
+ * if malloc fails, the malloc_checked function should cause normal
+ * process
+ * termination with a status value of 98
  */
 void *malloc_checked(unsigned int b)
 {
-	void *p;
+	int *p;
 
 	p = malloc(b);
 	if (p == NULL)
